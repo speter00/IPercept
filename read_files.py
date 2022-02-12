5b6a70ed74595817739064347ec77aa8d993ef8b
Sánta Péter <santapeter00@gmail.com>
2022-02-12 16:53:32 +0100

    Simplified column tidying, updated gitignore

--- read_files.py
+++ read_files.py
@@ -1,25 +1,22 @@
 import pandas as pd
 import utilities as ut
 
+# In order to make later operations simplier and cleaner,
+# it's best to separate the first columns (date-time) into 2 date, time columns
 
-data1 = pd.read_csv("test_files/spacedata1.csv", header=None)
-data2 = pd.read_csv("test_files/spacedata2.csv", header=None)
-data3 = pd.read_csv("test_files/spacedata3.csv", header=None)
-data4 = pd.read_csv("test_files/spacedata4.csv", header=None)
+data1 = pd.read_csv("test_files/spacedata1.csv", sep=r'[ ,]', header=None, names=["Date", "Time", "Acceleration"])
+data2 = pd.read_csv("test_files/spacedata2.csv", sep=r'[ ,]', header=None, names=["Date", "Time", "Acceleration"])
+data3 = pd.read_csv("test_files/spacedata3.csv", sep=r'[ ,]', header=None, names=["Date", "Time", "Acceleration"])
+data4 = pd.read_csv("test_files/spacedata4.csv", sep=r'[ ,]', header=None, names=["Date", "Time", "Acceleration"])
 
 # this one contains some malformed rows with wrong separators
 # a solution for this is to accept semicolons, commas and left arrows as separators, multiple if necessary
 
-data5 = pd.read_csv("test_files/spacedata5.csv", sep=r'[;,<]+', header=None)
+data5 = pd.read_csv("test_files/spacedata5.csv", sep=r'[ ;,<]+', header=None, names=["Date", "Time", "Acceleration"])
 
-# In order to make later operations simplier and cleaner,
-# it's best to separate the first columns (date-time) into 2 date, time columns
 
-data1 = ut.tidy_columns(data1)
-data2 = ut.tidy_columns(data2)
-data3 = ut.tidy_columns(data3)
-data4 = ut.tidy_columns(data4)
-data5 = ut.tidy_columns(data5)
+
+
 
 
 

acb40d2bb6bb4f48af91a9d5ca221fd4d162273a
Sánta Péter <santapeter00@gmail.com>
2022-02-13 18:10:33 +0100

    Smoothed data, added peak searching

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
 import pandas as pd
+import stumpy as stumpy
+
 from read_files import read_data
 import utilities as ut
+from matplotlib import pyplot as plt
 
-data: pd.Series = read_data()  
+from scipy.signal import savgol_filter
+
+import numpy as np
+
+data: pd.Series = read_data()
 data = ut.interpolate_outliers(data)
 
+# smoothing the data
+# Note: could experiment with different filtering methods, with different parameters
+
+data[:] = savgol_filter(data, 55, 3)
+
+peaks = ut.peak_finder(data)
 
 
 summary = dict()
@@ -20,6 +33,9 @@ summary["max"] = max_acc
 summary["avg"] = avg_acc
 summary["median"] = median_acc
 summary["std"] = std_acc
+
+
+
 
 
 
--- utilities.py
+++ utilities.py
@@ -1,5 +1,9 @@
+from typing import List, Tuple
+
 import pandas as pd
 from scipy import stats
+from scipy.signal import argrelextrema
+import numpy as np
 
 def interpolate_outliers(data: pd.Series) -> pd.Series:
     """
@@ -12,5 +16,22 @@ def interpolate_outliers(data: pd.Series) -> pd.Series:
     # Mask outlier values with nan, then pass them on for interpolation
 
     zscores = stats.zscore(data)
-    outliers = data[zscores[abs(zscores) > 3].index]
+    outliers = data[zscores[abs(zscores) > 3].index]  # both negative and positive outliers matter (thus the abs)
     return data.mask(data.isin(outliers)).interpolate(method="time")
+
+def peak_finder(data: pd.Series) -> pd.Series:
+    '''
+    Function for finding peaks in acceleration.
+
+    :param data: Input data.
+    :return: Peak accelerations with timestamps.
+    '''
+
+    # Note: local minimums are not low enough to have a high enough absolute Z-Score,
+    # but decreasing Z-Score threshold gives too many peaks. Some fine tuning could be needed.
+
+    zscores = stats.zscore(data)
+    extreme_zscores = zscores[abs(zscores) > 2]
+    ex_z_loc_maximums = extreme_zscores[argrelextrema(extreme_zscores.values, np.greater)[0]] # local maximums for extreme z-scores
+    max_peaks = data[ex_z_loc_maximums.index] # data, where timestamp matches that of a local max's timestamp
+    return max_peaks
